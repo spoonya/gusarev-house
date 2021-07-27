eeeb79cf7d07a0e5fad11058a06684061d1c074c
spoonya <nomad8t@gmail.com>
2021-07-27 13:30:01 +0300

    feat: add intersection observer polyfill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,10 @@ import * as sliders from './sliders';
 import { spoilerWebuild } from './spoilers';
 import { validateFormCatalog, validateFormCallback } from './forms';
 
+require('intersection-observer');
+
+IntersectionObserver.prototype.POLL_INTERVAL = 100;
+
 lazyLoadObserver.observe();
 
 validateFormCatalog();
