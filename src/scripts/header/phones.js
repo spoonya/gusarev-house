305832279a3813395ae5d3d8993ab144e428d84d
spoonya <nomad8t@gmail.com>
2021-08-06 11:21:34 +0300

    feat: add phones list click away event

--- src/scripts/header/phones.js
+++ src/scripts/header/phones.js
@@ -4,6 +4,17 @@ import { DOM, CLASSES } from '../constants';
 const phonesSubList = document.querySelector('#header-phones ul');
 const phonesArrow = document.querySelector('#header-phones svg');
 
+function onClickAway() {
+  document.addEventListener('click', (e) => {
+    if (!phonesSubList.classList.contains(CLASSES.active)) return;
+
+    if (e.target !== phonesArrow) {
+      phonesSubList.classList.remove(CLASSES.active);
+      phonesArrow.classList.remove(CLASSES.active);
+    }
+  });
+}
+
 function addClickEvtToArrow() {
   if (!phonesArrow) return;
 
@@ -16,9 +27,10 @@ function addClickEvtToArrow() {
 function controlPhonesList() {
   if (isMobile()) {
     DOM.body.classList.add('touch');
-  }
 
-  addClickEvtToArrow();
+    addClickEvtToArrow();
+    onClickAway();
+  }
 }
 
 export default controlPhonesList;
